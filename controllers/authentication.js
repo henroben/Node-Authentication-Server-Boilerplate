364cbb4c55a1b5adc8a7139b062cc720adf2a9da
Ben Mercer <ben@henroben.co.uk>
2017-03-16 16:16:30 +0000

    Error handling and saving for new user request

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,12 +8,32 @@ exports.signup = function(req, res, next) {
 
     // Check if user email already exits
     User.findOne({ email: email }, function(err, existingUser) {
-        
+        // Check for db connection error etc.
+        if(err) {
+            return next(err);
+        }
+
+        // If user does exist, return error
+        if(existingUser) {
+            return res.status(422).send({ error: 'Email is already registered'});
+        }
+
+        // If user does not exist, create user record
+        const user = new User({
+            email: email,
+            password: password
+        });
+        // Save user to database
+        user.save(function(err) {
+            if(err) {
+                return next(err);
+            }
+            // Respond to request indicating user created
+            res.json(user);
+        });
+
     });
 
-    // If user does exist, return error
 
-    // If user does not exist, create and save user record
 
-    // Respond to request indicating user created
 }
